c6e36c7323d5f877d0c33d93ed0d8708e1f71ef2
mark.woolston@csiro.au <mark.woolston@csiro.au@eb6df1c2-b7f6-0247-ee0a-37e5500bc369>
2011-02-16 22:19:59 +0000

    implement inherited defaults other progress

--- workforce/trunk/workforce/grails-app/services/au/org/ala/workforce/DataLoaderService.groovy
+++ workforce/trunk/workforce/grails-app/services/au/org/ala/workforce/DataLoaderService.groovy
@@ -3,13 +3,15 @@ package au.org.ala.workforce
 import groovy.sql.Sql
 import grails.converters.deep.JSON
 import org.xml.sax.SAXException
-import groovy.util.slurpersupport.NodeChild
-import groovy.util.slurpersupport.NodeChildren
 
 class DataLoaderService {
 
     static transactional = false
     javax.sql.DataSource dataSource
+
+    /**
+     * Load from XML metadata
+     */
 
     def loadQuestionSetXML(text) {
         println "LoadQuestionSetXML............."
@@ -24,20 +26,16 @@ class DataLoaderService {
             println e
             return
         }
-        //println "class=" + qset.getClass()
-        //println "size=" + qset.size()
-        println "name=" + qset.name()
-
         // clear existing
         def sql = new Sql(dataSource)
         sql.execute("delete from question")
 
-        loadXmlQuestions(qset.question, 1, 0, 0)
+        loadXmlQuestions(qset.question, 1, 0, 0, [:])
     }
 
-    private void loadXmlQuestions(questions, level, level1, level2) {
+    private void loadXmlQuestions(questions, level, level1, level2, Map defaults) {
         questions.eachWithIndex { it, idx ->
-            println "Level ${level}: ${it.text}"
+            //println "Level ${level}: ${it.text}"
             def l1, l2, l3
             switch (level) {
                 case 1: l1 = idx + 1; l2 = 0; l3 = 0; break;
@@ -45,45 +43,23 @@ class DataLoaderService {
                 case 3: l1 = level1; l2 = level2; l3 = idx + 1; break;
             }
             Question q = new Question(level1: l1, level2: l2, level3: l3)
-            //println it
-            //println "it.class = " + it.getClass()
-            //println "name=" + it.name
-            //println "instruction = " + it.@instruction
-            //println "qtype = " + it.@type
+            // reset defaults for new top level question
+            if (level == 1) {
+                defaults = [:]
+            }
+            defaults = setDefaults(defaults, it)
+
             q.instruction = it.@instruction
             q.qtype = it.@type.toString() ? QuestionType.valueOf(it.@type.toString()) : QuestionType.none
-            q.label = it.@label
+            q.label = it.label
             q.layoutHint = it.layoutHint
-            q.displayHint = it.displayHint
-            q.qdata = it.data
+            q.displayHint = valueOrDefault(it.displayHint, defaults)
+            q.qdata = extractJsonString(it.data) as grails.converters.JSON
             q.qtext = it.text
-
-            //println "datatype = " + it.answer?.@dataType
-            q.datatype = it.answer?.@dataType?.toString() ? AnswerDataType.valueOf(it.answer?.@dataType?.toString()) : AnswerDataType.text
-            q.alabel = it.answer?.@label
-            q.atype = (it.answer?.@type as String) ? AnswerType.valueOf(it.answer?.@type as String) : AnswerType.none
-            if (it.answer?.data) {
-                def map = transformToObject(it.answer?.data)
-                if (map?.data) {
-                    println ">>>adata>>>" + map
-                    /*map.each { key, value ->
-                        println ">${key} = ${value}"
-                    }*/
-                    println ">>>data " + map.data
-                    if (map.data instanceof String) {
-                        q.adata = map.data
-                    } else {
-                        def json = (map.data as grails.converters.JSON).toString()
-                        println json
-                        try {
-                            q.adata = json
-                        } catch (MissingPropertyException e) {
-                            println e
-                            q.adata = json
-                        }
-                    }
-                }
-            }
+            q.datatype = valueOrDefault(it.answer?.@dataType, defaults) ? AnswerDataType.valueOf(valueOrDefault(it.answer?.@dataType, defaults) as String) : AnswerDataType.text
+            q.atype = valueOrDefault(it.answer?.@type, defaults) ? AnswerType.valueOf(valueOrDefault(it.answer?.@type, defaults) as String) : AnswerType.none
+            q.adata = extractJsonString(it.answer?.data) as grails.converters.JSON
+            q.alabel = it.answer?.label.text()
 
             q.save()
             if (q.hasErrors()) {
@@ -92,69 +68,89 @@ class DataLoaderService {
                 }
             }
             if (it.question) {
-                loadXmlQuestions(it.question, level + 1, l1, l2)
+                loadXmlQuestions(it.question, level + 1, l1, l2, defaults)
             }
+        }
+    }
+
+    def extractJsonString(data) {
+        if (!data || !data.size()) {
+            return null
+        }
+        def result = null
+
+        // check for single string
+        if (data.children().size() == 0 && data.text()) {
+            return [data.text()]
+        }
+
+        // determine if it's a list or set of properties
+        def name = data.children()[0].name()
+        boolean isList = data.children().size() > 1
+        if (isList) {
+            data.children().each {
+                if (it.name() != name) {
+                    isList = false
+                }
+            }
+        }
+        
+        if (isList) {
+            //println "Processing list"
+            result = []
+            data.children().each {
+                //println "item=${it.text()}"
+                result << it.text()
+            }
+        } else {
+            //println "Processing object"
+            result = [:]
+            use(MapUtil) {
+                data.children().each {
+                    //println "prop=${it.name()}=${it.text()}"
+                    if (it.children().size()) {
+                        result.addString(it, extractJsonString(it))
+                    } else {
+                        result.add(it)
+                    }
+                }
+            }
+        }
+
+        return result
+    }
+
+    def Map setDefaults(defaults, node) {
+        if (node.@defaultAnswerType.text()) {
+            //println "Setting default answerType to ${node.@defaultAnswerType.text()}"
+            defaults.defaultType = node.@defaultAnswerType.text()
+        }
+        if (node.@defaultDataType.text()) {
+            //println "Setting default dataType to ${node.@defaultDataType.text()}"
+            defaults.defaultDataType = node.@defaultDataType.text()
+        }
+        if (node.@defaultDisplayHint.text()) {
+            //println "Setting default displayHint to ${node.@defaultDisplayHint.text()}"
+            defaults.defaultDisplayHint = node.@defaultDisplayHint.text()
+        }
+        return defaults
+    }
+
+    def valueOrDefault(node, defaults) {
+        def name = node.name()
+        //defaults.each { key, value -> println "${key}=${value}"}
+        if (node?.text()) {
+            node.text()
+        } else if (defaults."default${name[0].toUpperCase()+name.substring(1)}") {
+            return defaults."default${name[0].toUpperCase()+name.substring(1)}"
+        } else {
+            return null
         }
     }
 
     /**
-     * Assuming here we have no mixed content, ie content is either text or a node list
-     * and no attributes.
-     * @param node
-     * @return JSON object
+     * Load from JSON metadata
      */
-    def transformToObject(node) {
-        def map = [:]
-        if (node.childNodes()) {
-            map."${node.name().toString()}" = transformToList(node.childNodes())
-        } else {
-            map."${node.name()}" = [node.text()]
-        }
-        return map
-    }
-
-    def transformToList(children) {
-        def list = []
-        children.each {
-            list << transformToObject(it)
-        }
-        return list
-    }
-    
-    def debugNode(node, int depth) {
-        if (!node) { return null }
-        def indent = ""
-        depth.times { indent += "  "}
-
-        if (node.getClass().getSimpleName() == 'NodeChildren') {
-            def list = []
-            node.each {
-                list << debugNode(it, depth + 1)
-            }
-            return list
-        } else {
-            def map = [:]
-            println indent + "{" + node.name() + ":"
-            def objName = node.name()
-            def objList = [:]
-            /*node.attributes().each { key, value ->
-                println indent + "  ${key}:${value},"
-                objList."${key}" = value
-            }*/
-            def children = node.childNodes()
-            if (children) {
-                children.each {
-                    objList << debugNode(it, depth + 1)
-                }
-            } else {
-                if (node.text()) {
-                    println indent + node.text()
-                    objList = node.text()
-                }
-            }
-            return objList
-        }
-    }
 
     def loadQuestionSet(text) {
         def imp = JSON.parse(text)
@@ -196,4 +192,52 @@ class DataLoaderService {
             }
         }
     }
+
+    def loadTestXML(text) {
+        def model
+
+        try {
+            model = new XmlSlurper().parseText(text)
+        } catch (IOException e) {
+            println e
+            return
+        } catch (SAXException e) {
+            println e
+            return
+        }
+
+        println "text=" + model.text()
+        println "no. data chilren is ${model.data.children().size()}"
+    }
+
+}
+
+class MapUtil {
+    def static addInt(map, node) {
+        if (node.size()) {
+            map."${node.name()}" = node.text() as int
+        }
+    }
+    def static addString(map, node) {
+        if (node.size()) {
+            map."${node.name()}" = node.text()
+        }
+    }
+    def static addString(map, node, text) {
+        if (node.size()) {
+            map."${node.name()}" = text
+        }
+    }
+    def static add(map, node) {
+        if (node.size()) {
+            if (node.@value == 'int') {
+                //println "Processing ${node.name()} as int"
+                addInt(map, node)
+            } else {
+                //println "Processing ${node.name()} as string"
+                addString(map, node)
+            }
+        }
+    }
+
 }
