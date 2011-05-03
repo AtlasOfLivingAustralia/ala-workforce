172561f8561128d5b362b774353f5e8a3ea2b386
mark.woolston@csiro.au <mark.woolston@csiro.au@eb6df1c2-b7f6-0247-ee0a-37e5500bc369>
2011-05-03 22:50:58 +0000

    handle multiple question sets

--- workforce/trunk/workforce/grails-app/conf/UrlMappings.groovy
+++ workforce/trunk/workforce/grails-app/conf/UrlMappings.groovy
@@ -7,11 +7,11 @@ class UrlMappings {
 			}
 		}
 
-        "/question/load/xml" (controller: 'question', action: 'loadQuestionSetXML')
-        "/question/load/json" (controller: 'question', action: 'loadQuestionSet')
-        "/question/$id" (controller:'question', action:'singleQuestion')
-        "/questions/$from?" (controller:'question', action:'questions')
-        "/questions/$from?/$to?" (controller:'question', action:'questions')
+        "/question/load/xml/$set" (controller: 'question', action: 'loadQuestionSetXML')
+        "/question/load/json/$set" (controller: 'question', action: 'loadQuestionSet')
+        "/question/$set/$id" (controller:'question', action:'singleQuestion')
+        "/questions/$set/$from?" (controller:'question', action:'questions')
+        "/questions/$set/$from?/$to?" (controller:'question', action:'questions')
 
 		"/"(view:"/index")
 		"500"(view:'/error')
