95108acbe2ac285e2ba99dc847afc1181de3594b
mark.woolston <mark.woolston@csiro.au>
2011-05-03 22:50:08 +0000

    load 2 question sets

--- workforce/trunk/workforce/grails-app/conf/BootStrap.groovy
+++ workforce/trunk/workforce/grails-app/conf/BootStrap.groovy
@@ -3,7 +3,9 @@ class BootStrap {
     def dataLoaderService
 
     def init = { servletContext ->
-        dataLoaderService.loadQuestionSet(servletContext.getResource('metadata/question-set.xml').text)
+        dataLoaderService.clearQuestionSets()
+        dataLoaderService.loadQuestionSetXML(servletContext.getResource('metadata/question-set-1.xml').text, 1)
+        dataLoaderService.loadQuestionSetXML(servletContext.getResource('metadata/question-set-2.xml').text, 2)
     }
 
     def destroy = {
