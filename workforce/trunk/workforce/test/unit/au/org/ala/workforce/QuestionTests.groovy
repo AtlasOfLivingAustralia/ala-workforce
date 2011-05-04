66dd7eea707078a34fbd02623e0c29ae0d6117b7
mark.woolston@csiro.au <mark.woolston@csiro.au@eb6df1c2-b7f6-0247-ee0a-37e5500bc369>
2011-05-04 04:03:25 +0000

    test hash

--- workforce/trunk/workforce/test/unit/au/org/ala/workforce/QuestionTests.groovy
+++ workforce/trunk/workforce/test/unit/au/org/ala/workforce/QuestionTests.groovy
@@ -11,7 +11,9 @@ class QuestionTests extends GrailsUnitTestCase {
         super.tearDown()
     }
 
-    void testSomething() {
-
+    void testHash() {
+        assert new Question(qset: 1, level1: 1, level2: 1, level3: 1).makeHash() == 16843009
+        assert new Question(qset: 1, level1: 14, level2: 1, level3: 5).makeHash() == 17694981
+        assert new Question(qset: 20, level1: 200, level2: 200, level3: 200).makeHash() == 348702920
     }
 }
