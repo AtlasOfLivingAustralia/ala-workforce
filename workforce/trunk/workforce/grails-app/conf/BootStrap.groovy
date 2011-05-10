71f7befcb4c72693979db1749e993192185ea700
mark.woolston <mark.woolston@csiro.au>
2011-05-10 22:27:14 +0000

    use question set metadata to derive set number

--- workforce/trunk/workforce/grails-app/conf/BootStrap.groovy
+++ workforce/trunk/workforce/grails-app/conf/BootStrap.groovy
@@ -4,8 +4,8 @@ class BootStrap {
 
     def init = { servletContext ->
         dataLoaderService.clearQuestionSets()
-        dataLoaderService.loadQuestionSetXML(servletContext.getResource('metadata/question-set-1.xml').text, 1)
-        dataLoaderService.loadQuestionSetXML(servletContext.getResource('metadata/question-set-2.xml').text, 2)
+        dataLoaderService.loadQuestionSetXML(servletContext.getResource('metadata/question-set-1.xml').text)
+        dataLoaderService.loadQuestionSetXML(servletContext.getResource('metadata/question-set-2.xml').text)
     }
 
     def destroy = {
