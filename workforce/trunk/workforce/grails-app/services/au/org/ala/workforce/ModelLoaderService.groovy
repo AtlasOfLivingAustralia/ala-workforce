b9ebe5130d979e4bbea42b0c8281fa89cb35683d
mark.woolston <mark.woolston@csiro.au>
2011-05-12 22:52:55 +0000

    inject previous answers from database

--- workforce/trunk/workforce/grails-app/services/au/org/ala/workforce/ModelLoaderService.groovy
+++ workforce/trunk/workforce/grails-app/services/au/org/ala/workforce/ModelLoaderService.groovy
@@ -13,6 +13,15 @@ class ModelLoaderService {
      */
     def loadQuestion(int set, int questionNumber) {
 
+        /*
+         * Questions are stored as a flat list in the database.
+         *
+         * Relationships are implicit in the 3 levels of question numbering.
+         *
+         * This method builds the specified question and its sub-questions into
+         * a hierarchy of question models.
+         */
+
         // load all question records for this top-level question
         def records = Question.findAllByLevel1AndQset(questionNumber, set)
 
@@ -44,5 +53,50 @@ class ModelLoaderService {
         }
 
         return qtop
+    }
+
+    /**
+     * Load metadata for the question and inject the most recent answer for this user.
+     *
+     * @param set the question set
+     * @param questionNumber the question
+     * @param userId the user
+     * @return question model with latest answer (if any)
+     */
+    def loadQuestionWithAnswer(int set, int questionNumber, int userId) {
+
+        // load the question model
+        QuestionModel model = loadQuestion(set, questionNumber)
+        assert model
+
+        // find the answers
+        loadAnswers(model, userId)
+
+        return model
+    }
+
+    /**
+     * Find and inject the latest answer for the question and all its child questions.
+     *
+     * @param model the question model
+     * @param userId the user
+     * @return model populated with most recent answers
+     */
+    def loadAnswers(QuestionModel model, int userId) {
+        // get all answers for the question for this user
+        def answers = Answer.findAllByUserIdAndQuestionId(userId, model.hash, [sort:'lastUpdated',order:'desc'])
+
+        if (answers) {
+            // select the most recent answer
+            def answer = answers[0]
+
+            // inject answer into question model
+            model.setAnswerValueStr(answer.answerValue)
+        }
+
+        // call recursively for child questions
+        model.questions.each {
+            loadAnswers(it, userId)
+        }
     }
 }
