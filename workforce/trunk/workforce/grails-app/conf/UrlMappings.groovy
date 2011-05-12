83fc7d9c1c7c747276cbf4a950956a862af1280d
mark.woolston@csiro.au <mark.woolston@csiro.au@eb6df1c2-b7f6-0247-ee0a-37e5500bc369>
2011-05-12 22:56:30 +0000

    change to page-based requests remove json load url

--- workforce/trunk/workforce/grails-app/conf/UrlMappings.groovy
+++ workforce/trunk/workforce/grails-app/conf/UrlMappings.groovy
@@ -7,11 +7,10 @@ class UrlMappings {
 			}
 		}
 
+        "/set/$set/page/$page" (controller: 'question', action: 'page')
+
         "/question/load/xml/$set" (controller: 'question', action: 'loadQuestionSetXML')
-        "/question/load/json/$set" (controller: 'question', action: 'loadQuestionSet')
         "/question/$set/$id" (controller:'question', action:'singleQuestion')
-        "/questions/$set/$from?" (controller:'question', action:'questions')
-        "/questions/$set/$from?/$to?" (controller:'question', action:'questions')
 
 		"/"(view:"/index")
 		"500"(view:'/error')
