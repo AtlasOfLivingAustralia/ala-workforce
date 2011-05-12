81e12e376965cbad62286adf67fbda80cdc7240a
mark.woolston@csiro.au <mark.woolston@csiro.au@eb6df1c2-b7f6-0247-ee0a-37e5500bc369>
2011-05-12 22:55:12 +0000

    make answerValue nullable so we can store blank answers (where they are needed to mask earlier non-blank answers)

--- workforce/trunk/workforce/grails-app/domain/au/org/ala/workforce/Answer.groovy
+++ workforce/trunk/workforce/grails-app/domain/au/org/ala/workforce/Answer.groovy
@@ -11,12 +11,26 @@ class Answer {
     // the user id is a key to the user table
     int userId
 
-    // the timestamp reflects when the answer was stored - not that Grails handles this automagically based on the property name
+    // the timestamp reflects when the answer was stored - note that Grails handles this automagically based on the property name
     Date lastUpdated
 
-    // all properties are required
+    // all properties are required but the answer may be blank
     static constraints = {
+        answerValue(nullable: true)
     }
 
-    
+    /**
+     *
+     * Business rule: only save the answer if the answer value has changed from the most recent answer.
+     *
+    def beforeInsert = { event ->
+        // find the most recent answer to this question by this user
+        def answers = Answer.findAllByUserIdAndQuestionId(userId, questionId, [sort:'lastUpdated',order:'desc'])
+        if (answers && answers[0].answerValue == answerValue) {
+            println "cancelling save"
+            discard()
+            return false
+        }
+    }
+     */
 }
