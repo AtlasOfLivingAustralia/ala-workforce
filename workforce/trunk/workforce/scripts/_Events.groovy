db504856deffb9627f5771e3f1b97706b2645077
mark.woolston <mark.woolston@csiro.au>
2011-05-13 00:49:23 +0000

    add CAS security

---
+++ workforce/trunk/workforce/scripts/_Events.groovy
@@ -0,0 +1,13 @@
+import org.codehaus.groovy.grails.commons.ConfigurationHolder
+
+eventWebXmlStart = {
+    if (!ConfigurationHolder.config.security.cas.bypass) {
+        def tmpWebXml = "${projectWorkDir}/web.xml.tmp"
+        ant.replace(file: tmpWebXml, token: "@security.cas.serverName@", value: ConfigurationHolder.config.security.cas.serverName)
+        println "Injecting CAS Security Configuration: serverName = ${ConfigurationHolder.config.security.cas.serverName}"
+        ant.replace(file: tmpWebXml, token: "@security.cas.contextPath@", value: ConfigurationHolder.config.security.cas.contextPath)
+        println "Injecting CAS Security Configuration: contextPath = ${ConfigurationHolder.config.security.cas.contextPath}"
+        ant.replace(file: tmpWebXml, token: "/substitute-me", value: ConfigurationHolder.config.security.cas.urlPattern)
+        println "Injecting CAS Security Configuration: url pattern = ${ConfigurationHolder.config.security.cas.urlPattern}"
+    }
+}
