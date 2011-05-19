c3e9320040871f05e39a83f7efce9539d3959927
mark.woolston <mark.woolston@csiro.au>
2011-05-19 23:26:40 +0000

    support reports on specific users

--- workforce/trunk/workforce/grails-app/controllers/au/org/ala/workforce/ReportController.groovy
+++ workforce/trunk/workforce/grails-app/controllers/au/org/ala/workforce/ReportController.groovy
@@ -3,7 +3,6 @@ package au.org.ala.workforce
 class ReportController {
 
     def modelLoaderService
-    def scaffold = true
 
     /**
      * Display questions that have answers.
@@ -15,17 +14,19 @@ class ReportController {
         def questionId = 1;
         def setId = params.set.toInteger() ?: 1
         def qset = QuestionSet.findBySetId(setId)
+        def userid = params.id as int ?: 1
+        def user = User.findByUserid(userid)
 
         def questions = Question.findAllByLevel2AndQset(0, setId)
 
         questions.each {
-            questionList <<  modelLoaderService.loadQuestionWithAnswer(setId, it.level1, 1)
+            questionList <<  modelLoaderService.loadQuestionWithAnswer(setId, it.level1, userid)
         }
 
         assert questionList
 
         // render the page
-        render(view:'answers', model:[qset: qset, questions: questionList])
+        [qset: qset, questions: questionList, user: user]
     }
 
     /**
