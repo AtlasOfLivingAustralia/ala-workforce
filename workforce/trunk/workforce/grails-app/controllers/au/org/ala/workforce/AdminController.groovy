8edb85a689438ac44336190ccc132e7b41491777
peter.flemming@csiro.au <peter.flemming@csiro.au@eb6df1c2-b7f6-0247-ee0a-37e5500bc369>
2011-05-26 04:44:09 +0000

    Initial version

---
+++ workforce/trunk/workforce/grails-app/controllers/au/org/ala/workforce/AdminController.groovy
@@ -0,0 +1,16 @@
+package au.org.ala.workforce
+
+class AdminController {
+
+    def modelLoaderService
+
+    def dashboard = {
+
+        def setId = params.set.toInteger() ?: 1
+        def qset = QuestionSet.findBySetId(setId)
+        def userId = ((org.jasig.cas.client.authentication.AttributePrincipal) request.getUserPrincipal()).getAttributes().get("userid")
+
+        // render the page
+        render(view:'dashboard', model:[qset: qset, user: userId])
+    }
+}
