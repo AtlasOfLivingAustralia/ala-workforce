3c3f26357bf72085c90f90638cf56cf64f1a6dc5
peter.flemming <peter.flemming@csiro.au>
2011-05-31 08:04:06 +0000

    Added additional CAS config items

--- workforce/trunk/workforce/scripts/_Events.groovy
+++ workforce/trunk/workforce/scripts/_Events.groovy
@@ -3,6 +3,8 @@ import org.codehaus.groovy.grails.commons.ConfigurationHolder
 eventWebXmlStart = {
     if (!ConfigurationHolder.config.security.cas.bypass) {
         def tmpWebXml = "${projectWorkDir}/web.xml.tmp"
+        ant.replace(file: tmpWebXml, token: "@security.cas.casServerName@", value: ConfigurationHolder.config.security.cas.casServerName)
+        println "Injecting CAS Security Configuration: casServerName = ${ConfigurationHolder.config.security.cas.casServerName}"
         ant.replace(file: tmpWebXml, token: "@security.cas.serverName@", value: ConfigurationHolder.config.security.cas.serverName)
         println "Injecting CAS Security Configuration: serverName = ${ConfigurationHolder.config.security.cas.serverName}"
         ant.replace(file: tmpWebXml, token: "@security.cas.contextPath@", value: ConfigurationHolder.config.security.cas.contextPath)
@@ -11,5 +13,9 @@ eventWebXmlStart = {
         println "Injecting CAS Security Configuration: url pattern = ${ConfigurationHolder.config.security.cas.urlPattern}"
         ant.replace(file: tmpWebXml, token: "@security.cas.urlExclusionPattern@", value: ConfigurationHolder.config.security.cas.urlExclusionPattern)
         println "Injecting CAS Security Configuration: url exclusion pattern = ${ConfigurationHolder.config.security.cas.urlExclusionPattern}"
+        ant.replace(file: tmpWebXml, token: "@security.cas.authenticateOnlyIfLoggedInPattern@", value: ConfigurationHolder.config.security.cas.authenticateOnlyIfLoggedInPattern)
+        println "Injecting CAS Security Configuration: authenticate only if logged in pattern = ${ConfigurationHolder.config.security.cas.authenticateOnlyIfLoggedInPattern}"
+        ant.replace(file: tmpWebXml, token: "@security.cas.loginUrl@", value: ConfigurationHolder.config.security.cas.loginUrl)
+        println "Injecting CAS Security Configuration: loginUrl = ${ConfigurationHolder.config.security.cas.loginUrl}"
     }
 }
