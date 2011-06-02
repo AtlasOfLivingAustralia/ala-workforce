77653b5b0385919bb66df8a86e9f6be4f40c5b75
mark.woolston@csiro.au <mark.woolston@csiro.au@eb6df1c2-b7f6-0247-ee0a-37e5500bc369>
2011-06-02 05:33:33 +0000

    add timestamp helper method to insert a completion event helper method to get number of surveys complete for a year

--- workforce/trunk/workforce/grails-app/domain/au/org/ala/workforce/Event.groovy
+++ workforce/trunk/workforce/grails-app/domain/au/org/ala/workforce/Event.groovy
@@ -8,9 +8,21 @@ class Event {
     int userid                      // the user
     int setId                       // the question set
     String name                     // the type of event
+    // the timestamp reflects when the answer was stored - note that Grails handles this automagically based on the property name
+    Date lastUpdated
 
     static constraints = {
         name(maxSize:45, inList: [STARTED, COMPLETE])
     }
 
+    static complete(userid, set) {
+        new Event(userid: userid, setId: set, name: COMPLETE).save()
+    }
+
+    static def numberCompletedForYear(int setId, String year) {
+        def yearStart = year + "-01-01"
+        def count = Event.executeQuery("select count(distinct id) from Event where setId = :set and last_updated >= :year and name = 'complete'",
+            [set: setId, year: yearStart])
+        return count[0]
+    }
 }
