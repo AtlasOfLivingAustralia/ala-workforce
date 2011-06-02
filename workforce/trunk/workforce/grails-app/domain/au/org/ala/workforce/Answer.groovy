e75785f33ef84e84506a5b8c5d6ccc1131aed671
mark.woolston@csiro.au <mark.woolston@csiro.au@eb6df1c2-b7f6-0247-ee0a-37e5500bc369>
2011-06-02 05:32:46 +0000

    add setId for searching helper method to get the number of surveys started in a year

--- workforce/trunk/workforce/grails-app/domain/au/org/ala/workforce/Answer.groovy
+++ workforce/trunk/workforce/grails-app/domain/au/org/ala/workforce/Answer.groovy
@@ -8,6 +8,9 @@ class Answer {
     // the question identifier is the question's assigned guid
     String guid
 
+    // the question set
+    int setId
+    
     // the user id is a key to the user table
     int userId
 
@@ -18,6 +21,13 @@ class Answer {
     static constraints = {
         guid(maxSize:36)
         answerValue(nullable: true)
+    }
+
+    static def numberStartedForYear(int setId, String year) {
+        def yearStart = year + "-01-01"
+        def count = Event.executeQuery("select count(distinct userId) from Answer where setId = :set and last_updated >= :year",
+            [set: setId, year: yearStart])
+        return count[0]
     }
 
 }
