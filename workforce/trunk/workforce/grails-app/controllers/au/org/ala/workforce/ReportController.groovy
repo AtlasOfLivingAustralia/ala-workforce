3afc79f9f43dc3fcec96e505cbec2fe232fa6217
peter.flemming@csiro.au <peter.flemming@csiro.au@eb6df1c2-b7f6-0247-ee0a-37e5500bc369>
2011-06-06 04:42:25 +0000

    Bug fixes

--- workforce/trunk/workforce/grails-app/controllers/au/org/ala/workforce/ReportController.groovy
+++ workforce/trunk/workforce/grails-app/controllers/au/org/ala/workforce/ReportController.groovy
@@ -16,18 +16,18 @@ class ReportController {
         def qset = QuestionSet.findBySetId(setId)
 
         def loggedInUserId = request.userPrincipal.attributes.userid as int
-        def userid = params.id as int ?: loggedInUserId
-        if (userid != loggedInUserId && !request.isUserInRole('ROLE_ABRS_ADMIN')) {
-            userid = loggedInUserId
+        def userId = params.id as int ?: loggedInUserId
+        if (userId != loggedInUserId && !request.isUserInRole('ROLE_ABRS_ADMIN')) {
+            userId = loggedInUserId
         }
-        def user = User.findByUserid(userid)
+        def user = User.findByUserid(userId)
 
         def questions = Question.findAllByLevel2AndQset(0, setId)
 
         def year = DateUtil.getYear(params.year)
         def answers = Answer.getAnswers(setId, userId, year)
         questions.each {
-            questionList <<  modelLoaderService.loadQuestionWithAnswer(setId, it.level1, userid, year)
+            questionList <<  modelLoaderService.loadQuestionWithAnswer(setId, it.level1, answers)
         }
 
         assert questionList
@@ -47,11 +47,11 @@ class ReportController {
         def setId = params.set.toInteger() ?: 1
         def qset = QuestionSet.findBySetId(setId)
         def loggedInUserId = request.userPrincipal.attributes.userid as int
-        def userid = params.id as int ?: loggedInUserId
-        if (userid != loggedInUserId && !request.isUserInRole('ROLE_ABRS_ADMIN')) {
-            userid = loggedInUserId
+        def userId = params.id as int ?: loggedInUserId
+        if (userId != loggedInUserId && !request.isUserInRole('ROLE_ABRS_ADMIN')) {
+            userId = loggedInUserId
         }
-        def user = User.findByUserid(userid)
+        def user = User.findByUserid(userId)
         def year = DateUtil.getYear(params.year)
         def answers = Answer.getAnswers(setId, userId, year)
         def question = modelLoaderService.loadQuestionWithAnswer(setId, questionId, answers)
