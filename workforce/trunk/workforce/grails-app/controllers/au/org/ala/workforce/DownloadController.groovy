b53b5aaa0b9ed4fa8725c759507ecb5b4cfdd52a
peter.flemming@csiro.au <peter.flemming@csiro.au@eb6df1c2-b7f6-0247-ee0a-37e5500bc369>
2011-06-07 07:19:33 +0000

    Controller for downloading csv data

---
+++ workforce/trunk/workforce/grails-app/controllers/au/org/ala/workforce/DownloadController.groovy
@@ -0,0 +1,101 @@
+package au.org.ala.workforce
+
+import au.com.bytecode.opencsv.CSVWriter
+
+class DownloadController {
+
+    def modelLoaderService
+
+    def download = {
+
+        def setId = params.set as int ?: 1
+        def qset = QuestionSet.findBySetId(setId)
+        def year = DateUtil.getYear(params.year)
+
+        String csvResultFile = "ABRS ${qset.shortName} ${year}.csv";
+        response.setHeader("Cache-Control", "must-revalidate");
+        response.setHeader("Content-Disposition", "attachment;filename=" + csvResultFile);
+        response.setContentType("text/plain");
+        final CSVWriter csvWriter = new CSVWriter(new OutputStreamWriter(response.getOutputStream()));
+
+        def topLevelQuestions = Question.findAllByLevel2AndQset(0, setId)
+
+        // write header row
+        def questions = []
+        topLevelQuestions.each { question ->
+            questions <<  modelLoaderService.loadQuestionWithAnswer(setId, question.level1, [:])
+        }
+        csvWriter.writeNext(getHeaderRow(questions) as String[])
+
+        // write answers for each completed survey
+        def users = Event.usersCompletedForYear(setId, year as String)
+        users.each { userid ->
+            def answers = Answer.getAnswers(setId, userid, year)
+            def answerRow = [userid]
+            questions = []
+            topLevelQuestions.each { question ->
+                questions <<  modelLoaderService.loadQuestionWithAnswer(setId, question.level1, answers)
+            }
+            questions.each { model ->
+                addAnswer(model, answerRow)
+            }
+
+            csvWriter.writeNext(answerRow as String[])
+        }
+
+        csvWriter.close()
+        return null
+    }
+
+    List getHeaderRow(List questions) {
+        def headerRow = ['User']
+
+        questions.each {
+            addQuestionNumber(it, headerRow)
+        }
+
+        return headerRow
+    }
+
+    def addQuestionNumber(QuestionModel model, List row) {
+        if (model.guid) {
+            switch (model.level) {
+                case 1:
+                    row << "${model.questionNumber}"
+                    break
+                case 2:
+                    row << "${model.owner.questionNumber}.${model.questionNumber}"
+                    break
+                case 3:
+                    row << "${model.owner.owner.questionNumber}.${model.owner.questionNumber}.${model.questionNumber}"
+                    break
+                default:
+                    row << '?'
+                    break
+            }
+        }
+
+        model.questions.each {
+            addQuestionNumber(it, row)
+        }
+    }
+
+    def addAnswer(QuestionModel model, List row) {
+        if (model.guid) {
+            if (model.answerValueStr) {
+                if (model.answerValueStr == 'on') {
+                    row << model.qtext
+                } else {
+                    row << model.answerValueStr
+                }
+            } else {
+                row << ''
+            }
+        }
+
+        model.questions.each {
+            addAnswer(it, row)
+        }
+    }
+}
+
