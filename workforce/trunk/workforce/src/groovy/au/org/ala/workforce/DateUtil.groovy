20a38f239bbc0a5b393946b5222d4fd017c586b7
peter.flemming@csiro.au <peter.flemming@csiro.au@eb6df1c2-b7f6-0247-ee0a-37e5500bc369>
2011-06-30 00:35:15 +0000

    Added getCurrentYear() and getNiceDateFromSqlDate()

--- workforce/trunk/workforce/src/groovy/au/org/ala/workforce/DateUtil.groovy
+++ workforce/trunk/workforce/src/groovy/au/org/ala/workforce/DateUtil.groovy
@@ -17,4 +17,18 @@ class DateUtil {
             return c.get(Calendar.YEAR)
         }
     }
+
+    static int getCurrentYear() {
+        def c = Calendar.instance
+        return c.get(Calendar.YEAR)
+    }
+
+    static String getNiceDateFromSqlDate(String date) {
+        def year = date.substring(0, 4)
+        def numMonth = date.substring(5, 7) as int
+        def month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'][numMonth - 1]
+        def day = date.substring(8, 10)
+        def time = date.substring(11, 16)
+        return "${day} ${month} ${year} ${time}"
+    }
 }
