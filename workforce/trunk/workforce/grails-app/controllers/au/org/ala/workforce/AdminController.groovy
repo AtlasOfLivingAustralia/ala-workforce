48defca2fa099d8ae38c732b63218b6e58c247ec
peter.flemming@csiro.au <peter.flemming@csiro.au@eb6df1c2-b7f6-0247-ee0a-37e5500bc369>
2011-08-01 12:35:05 +0000

    Question set constants

--- workforce/trunk/workforce/grails-app/controllers/au/org/ala/workforce/AdminController.groovy
+++ workforce/trunk/workforce/grails-app/controllers/au/org/ala/workforce/AdminController.groovy
@@ -7,14 +7,14 @@ class AdminController {
     def dashboard = {
         cache false
 
-        def setId = params.set.toInteger() ?: 1
+        def setId = params.set.toInteger() ?: QuestionModel.CURRENT_PERSONAL_SURVEY
         def qset = QuestionSet.findBySetId(setId)
         def currentYear = new GregorianCalendar().get(Calendar.YEAR).toString()
         def numberStarted = Answer.numberStartedForYear(setId, currentYear)
         def numberCompleted = Event.numberCompletedForYear(setId, currentYear)
         def total
         switch (setId) {
-            case 2: total = Institution.countBySetId(2); break
+            case QuestionModel.CURRENT_INSTITUTIONAL_SURVEY: total = Institution.countBySetId(setId); break
             default: total = ((numberCompleted/100 as int) + 1) * 100
         }
 
