450bff96a70e571f934952261346dd86714ffe13
peter.flemming <peter.flemming@csiro.au>
2011-08-09 00:29:49 +0000

    New type for Survey metadata

---
+++ workforce/trunk/workforce/src/groovy/au/org/ala/workforce/SurveyType.groovy
@@ -0,0 +1,14 @@
+package au.org.ala.workforce
+
+/**
+ * User: peterflemming
+ * Date: 3/08/11
+ * Time: 12:10 PM
+ */
+public enum SurveyType {
+    personal, institutional
+
+    static list() {
+        [personal, institutional]
+    }
+}
