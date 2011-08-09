c46771ae7c5910d3045263ddfa86facae27b3cdd
peter.flemming@csiro.au <peter.flemming@csiro.au@eb6df1c2-b7f6-0247-ee0a-37e5500bc369>
2011-08-09 00:34:46 +0000

    Text  substitution test

--- workforce/trunk/workforce/test/unit/au/org/ala/workforce/DataLoaderServiceTests.groovy
+++ workforce/trunk/workforce/test/unit/au/org/ala/workforce/DataLoaderServiceTests.groovy
@@ -1,6 +1,6 @@
 package au.org.ala.workforce
 
-import grails.test.*
+import grails.test.GrailsUnitTestCase
 
 class DataLoaderServiceTests extends GrailsUnitTestCase {
     protected void setUp() {
@@ -11,7 +11,14 @@ class DataLoaderServiceTests extends GrailsUnitTestCase {
         super.tearDown()
     }
 
-    void testSomething() {
-
+    void testSubstitutePlaceholders() {
+        def text = 'The first year = ${year1} and the second year = ${year2}'
+        def years = ['year1': '2009', 'year2': '2010']
+        assert 'The first year = 2009 and the second year = 2010' == substitutePlaceholders(text, years)
     }
+
+    String substitutePlaceholders(String text, Map priorYears) {
+        return text.replaceAll(/\$\{(\w+)\}/) { all, placeholder ->  priorYears[placeholder] }
+    }
+
 }
