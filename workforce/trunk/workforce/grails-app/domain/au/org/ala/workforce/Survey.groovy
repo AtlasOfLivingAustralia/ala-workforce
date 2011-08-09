8dbdc7eb67c96960d733e59816de9ba6870006ef
peter.flemming@csiro.au <peter.flemming@csiro.au@eb6df1c2-b7f6-0247-ee0a-37e5500bc369>
2011-08-09 08:08:11 +0000

    Made questtion_set_id the identity column.  Added init() to populate table if empty.

--- workforce/trunk/workforce/grails-app/domain/au/org/ala/workforce/Survey.groovy
+++ workforce/trunk/workforce/grails-app/domain/au/org/ala/workforce/Survey.groovy
@@ -4,32 +4,44 @@ class Survey {
 
     int year                        // Year of the survey
     SurveyType type                 // Survey type - currently Personal or Institutional
-    int questionSetId               // Question set associated with this survey
+    //int questionSetId               // Question set associated with this survey
     int priorYear1                  // Earlier prior year for funding or publications
     int priorYear2                  // Later prior year for funding or publications
 
-    static constraints = {
-        questionSetId(unique:true)
+    static mapping = {
+        table 'survey'
+        version false
+        id column: 'question_set_id'
+    }
+
+    static void init() {
+        def surveys = Survey.findAll()
+        if (surveys.size() == 0) {
+            Survey survey = new Survey(year: 2011, priorYear1: 2009, priorYear2: 2010, type: 'personal')
+            survey.save()
+            survey = new Survey(year: 2011, priorYear1: 2009, priorYear2: 2010, type: 'institutional')
+            survey.save()
+        }
     }
 
     static int getCurrentQSetId(SurveyType type) {
         int year = DateUtil.getCurrentYear()
         Survey s = Survey.findByYearAndType(year, type)
-        return s.questionSetId
+        return s.id
     }
 
     static Map getSurveyMetadata(int qset) {
-        Survey s = Survey.findByQuestionSetId(qset)
+        Survey s = Survey.findById(qset)
         return [year1: s.priorYear1, year2: s.priorYear2]
     }
 
     static String getDescription(int qset) {
-        Survey s = Survey.findByQuestionSetId(qset)
+        Survey s = Survey.findById(qset)
         return "${s.type}-${s.year}"
     }
 
     static String getType(int qset) {
-        Survey s = Survey.findByQuestionSetId(qset)
+        Survey s = Survey.findById(qset)
         return s.type
     }
 }
